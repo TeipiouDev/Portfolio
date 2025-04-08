4e41147af43a3f16588efc512bd98e5e7c245ba3
TeipiouDev <teipiou@gmail.com>
2025-04-08 20:16:06 +0300

    Add files via upload

---
+++ NES CHR Visualizer/NESCHRVisualizer/NESCHRVisualizer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NESCHRVisualizer
+{
+    class NESCHRVisualizer
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Tiedostoja ei annettu, anna vähintään yksi NES ROM-tiedosto!");
+                Console.WriteLine("Esimerkki: NESCHRVisualizer \"Final Fantasy.nes\" ");
+                Environment.Exit(1);
+            }
+
+            Color[] palette = { Color.Black, Color.Red, Color.Orange, Color.White };
+
+            string filePath;
+
+            if (args.Length != 0)
+            {
+
+                for (int a = 0; a < args.Count(); a++)
+                {
+
+                    filePath = args[a];
+                    List<NESSprite> sprites = new List<NESSprite>();
+
+                    //Luetaan kahdeksan tavua ja väritetään 1-bitit yhdellä värillä.
+                    //Luetaan seuraavat kahdeksan tavua ja väritetään 1-bitit toisella värillä
+                    //Jos molemmat bitit ovat 1, väritetään kolmannella värillä.
+
+                    long fileSize;
+
+                    try
+                    {
+                        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                        {
+
+                            Console.WriteLine("Käydään ROM-tiedostoa " + filePath + " läpi...");
+                            if (!ValidateHeader(fs))
+                            {
+                                Console.WriteLine("Tiedoston " + filePath + " otsikko ei vastaa iNES-formaattia.");
+                                continue;
+                            }
+
+                            fs.Position = 0x00000;
+
+                            fileSize = fs.Length;
+
+                            while (fs.Position < fs.Length)
+                            {
+                                NESSprite sprite = new NESSprite();
+
+                                sprite.Palette = palette;
+                                sprite.SetData(fs, (int)fs.Position);
+                                sprites.Add(sprite);
+                            }
+                        }
+
+                        Bitmap img = new Bitmap(1024, (int)fileSize / 256, PixelFormat.Format32bppArgb);
+
+                        Graphics g = Graphics.FromImage(img);
+
+                        g.CompositingMode = CompositingMode.SourceOver;
+
+                        int i = 0;
+                        for (int heightInBlocks = 0; heightInBlocks < (int)img.Height / 8; heightInBlocks++)
+                        {
+
+                            for (int widthInBlocks = 0; widthInBlocks < (int)img.Width / 8; widthInBlocks++)
+                            {
+
+                                g.DrawImage(sprites[i].GetBitmap(), widthInBlocks * 8, heightInBlocks * 8);
+                                i++;
+                            }
+                        }
+
+                        img.Save(Path.GetFileNameWithoutExtension(filePath) + ".png");
+
+                    }
+
+                    catch (FileNotFoundException)
+                    {
+
+                        Console.WriteLine("Tiedostoa " + filePath + " ei löytynyt.");
+                    }
+
+
+                }
+            }
+
+            Console.WriteLine("Valmis.");
+
+        }
+
+        //Stackoverflow-foorumeilta röyhkeästi pöllitty simppeli aliohjelma tarkistamaan, onko tavun tietty bitti 1 vai ei. 
+        static bool IsBitSet(byte b, int pos)
+        {
+            return (b & (1 << pos)) != 0;
+        }
+
+        //Tarkistetaan NES-tiedoston otsikko
+        //TODO: Tee tarkistus kunnolla
+        static bool ValidateHeader(FileStream fs)
+        {
+            
+            fs.Position = 0;
+
+            //NES-otsikko tiedosto on 16 tavun kokoinen, josta neljä ensimmäistä tavua sisältää kirjaimet N, E ja S ja viimeisenä MS-DOS rivinvaihdon
+            //Tarvittaessa laajennetaan tarkemmaksi tarkistukseksi.
+            if ((byte)fs.ReadByte() != 0x4E
+                || (byte)fs.ReadByte() != 0x45 
+                || (byte)fs.ReadByte() != 0x53 
+                || (byte)fs.ReadByte() != 0x1A)
+                return false;
+            
+
+            //PRG ROM koko 16384 tavun yksikköinä
+            int prg = fs.ReadByte();
+
+            //CHR ROM koko 8192 tavun yksikköinä, 0 = käytetään CHR RAM
+            int chr = fs.ReadByte();
+
+            byte mapper = (byte)fs.ReadByte();
+        
+    
+            return true;
+
+        }
+    }
+}
---
+++ NES CHR Visualizer/NESCHRVisualizer/NESSprite.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NESCHRVisualizer
+{
+    class NESSprite
+    {
+
+        private byte[] data = new byte[16];
+        private Color[] palette = new Color[4];
+        private Bitmap bmp = new Bitmap(8, 8);
+
+        //Muodostaa datasta kuvatiedoston.
+        public Bitmap GetBitmap()
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+
+                    if (IsBitSet(data[i], j) && IsBitSet(data[i + 8], j))
+                    {
+                        bmp.SetPixel(7 - j, i, palette[3]);
+                    }
+                    else if (IsBitSet(data[i], j) && !IsBitSet(data[i + 8], j))
+                    {
+                        bmp.SetPixel(7 - j, i, palette[1]);
+                    }
+                    else if (!IsBitSet(data[i], j) && IsBitSet(data[i + 8], j))
+                    {
+                        bmp.SetPixel(7 - j, i, palette[2]);
+                    }
+                    else
+                    {
+                        bmp.SetPixel(7 - j, i, palette[0]);
+                    }
+
+                }
+            }
+
+            return bmp;
+
+        }
+
+        //Tallentaa spriten tiedostoon, .bmp liite lisätään tiedoston perään.
+        public void SaveBitMap(string txtPath)
+        {
+            bmp.Save(txtPath + ".bmp");
+        }
+
+        //Jos taulukossa valmiina tavudata, laitetaan se spriten dataan.
+        public void SetData(byte[] d)
+        {
+            data = d;
+        }
+
+        /// <summary>
+        /// Lukee tiedostosta tavut talteen.
+        /// </summary>
+        /// <param name="txtPath">Tiedostopolku</param>
+        /// <param name="position">Mistä kohtaan tiedostoa aloitetaan lukeminen</param>
+        public void SetData(string txtPath, int position)
+        {
+            using (FileStream fs = new FileStream(txtPath, FileMode.Open, FileAccess.Read))
+            {
+
+                SetData(fs, position);
+
+            }
+        }
+
+        /// <summary>
+        /// Lukee tiedostovirrasta raakadatan talteen.
+        /// </summary>
+        /// <param name="fs">Tiedostovirta</param>
+        /// <param name="position">Mistä kohtaa tiedostovirtaa aloitetaan lukeminen</param>
+        public void SetData(FileStream fs, int position)
+        {
+            fs.Position = position;
+
+            for (int i = 0; i < 16; i++)
+            {
+                data[i] = (byte)fs.ReadByte();
+
+            }
+        }
+
+        public Color[] Palette
+        {
+            get { return palette; }
+            set { palette = value; }
+        }
+
+        //Stackoverflow-foorumeilta röyhkeästi pöllitty simppeli aliohjelma tarkistamaan, onko tavun tietty bitti 1 vai ei. 
+        private bool IsBitSet(byte b, int pos)
+        {
+            return (b & (1 << pos)) != 0;
+        }
+    }
+}
+
+
